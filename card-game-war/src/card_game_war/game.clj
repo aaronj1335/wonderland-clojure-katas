6a794b242916db92685c4f1e8792eb2ab04ee3be
Aaron Stacy <aaron.r.stacy@gmail.com>
2014-12-21 17:53:12 -0600

    this means war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,51 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 :jack :queen :king :ace])
 (def cards
-  (for [suit suits
-        rank ranks]
+  (for [rank ranks
+        suit suits]
     [suit rank]))
+(def ordering (into {} (map-indexed #(-> [%2 %1]) cards)))
 
-(defn play-round [player1-card player2-card])
+(defn deal
+  "shuffle and deal a sequence of cards among players (default 2)
 
-(defn play-game [player1-cards player2-cards])
+  try to mimic the order in which people actually deal, such that every player
+  gets a card on each iteration.
+
+  if the numbe of cards is not easily divisible by the number of players, just
+  leave out the extra cards."
+  ([cards]
+   (deal cards 2))
+  ([cards num-players]
+   (apply map vector (partition num-players (shuffle cards)))))
+
+(defn winner
+  "return the winning card based on `ordering`
+
+  works for a variable number of cards"
+  [& cards]
+  (->> cards
+       (map #(-> {:order (ordering %) :card %}))
+       (apply max-key :order)
+       (:card)))
+
+(defn play-round
+  "given each players deck, play a card from each and add them to the bottom of
+  the winner's deck"
+  [player1-cards player2-cards]
+  (let [card1 (first player1-cards)
+        card2 (first player2-cards)]
+    (if (= card1 (winner card1 card2))
+      [(concat (rest player1-cards) [card1 card2])
+       (rest player2-cards)]
+      [(rest player1-cards)
+       (concat (rest player2-cards) [card2 card1])])))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [[player1-cards player2-cards] (play-round player1-cards player2-cards)]
+    (cond
+      (nil? (seq player1-cards)) "Player 2 wins"
+      (nil? (seq player2-cards)) "Player 1 wins"
+      :else (recur (play-round player1-cards player2-cards)))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -3,18 +3,182 @@
             [card-game-war.game :refer :all]))
 
 
-;; fill in  tests for your game
+(deftest test-ordering
+  (is (= ordering {[:spade 2] 0
+                   [:club 2] 1
+                   [:diamond 2] 2
+                   [:heart 2] 3
+                   [:spade 3] 4
+                   [:club 3] 5
+                   [:diamond 3] 6
+                   [:heart 3] 7
+                   [:spade 4] 8
+                   [:club 4] 9
+                   [:diamond 4] 10
+                   [:heart 4] 11
+                   [:spade 5] 12
+                   [:club 5] 13
+                   [:diamond 5] 14
+                   [:heart 5] 15
+                   [:spade 6] 16
+                   [:club 6] 17
+                   [:diamond 6] 18
+                   [:heart 6] 19
+                   [:spade 7] 20
+                   [:club 7] 21
+                   [:diamond 7] 22
+                   [:heart 7] 23
+                   [:spade 8] 24
+                   [:club 8] 25
+                   [:diamond 8] 26
+                   [:heart 8] 27
+                   [:spade 9] 28
+                   [:club 9] 29
+                   [:diamond 9] 30
+                   [:heart 9] 31
+                   [:spade :jack] 32
+                   [:club :jack] 33
+                   [:diamond :jack] 34
+                   [:heart :jack] 35
+                   [:spade :queen] 36
+                   [:club :queen] 37
+                   [:diamond :queen] 38
+                   [:heart :queen] 39
+                   [:spade :king] 40
+                   [:club :king] 41
+                   [:diamond :king] 42
+                   [:heart :king] 43
+                   [:spade :ace] 44
+                   [:club :ace] 45
+                   [:diamond :ace] 46
+                   [:heart :ace] 47})))
+
+(deftest test-winner
+
+  (testing "higher numbers of the same suit beat lower numbers"
+    (is (= [:spade 3] (winner [:spade 3] [:spade 2])))
+    (is (= [:spade 3] (winner [:spade 2] [:spade 3]))))
+  (testing "higher numbers of lower suits beat lower numbers of higher suits"
+    (is (= [:spade 3] (winner [:spade 3] [:diamond 2])))
+    (is (= [:spade 3] (winner [:diamond 2] [:spade 3]))))
+  (testing "higher suits beat lower suits of equal number"
+    (is (= [:club :jack] (winner [:spade :jack] [:club :jack])))
+    (is (= [:club :jack] (winner [:club :jack] [:spade :jack]))))
+
+  (testing "queens are higher rank than jacks"
+    (is (= [:heart :queen] (winner [:heart :queen] [:heart :jack])))
+    (is (= [:heart :queen] (winner [:heart :jack] [:heart :queen]))))
+  (testing "kings are higher rank than queens"
+    (is (= [:heart :king] (winner [:heart :queen] [:heart :king])))
+    (is (= [:heart :king] (winner [:heart :king] [:heart :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (= [:heart :ace] (winner [:heart :ace] [:heart :king])))
+    (is (= [:heart :ace] (winner [:heart :king] [:heart :ace]))))
+
+  (testing "clubs are higher suit than spades"
+    (is (= [:club :queen] (winner [:club :queen] [:spade :queen])))
+    (is (= [:club :queen] (winner [:spade :queen] [:club :queen]))))
+  (testing "diamonds are higher suit than clubs"
+    (is (= [:diamond :queen] (winner [:diamond :queen] [:club :queen])))
+    (is (= [:diamond :queen] (winner [:club :queen] [:diamond :queen]))))
+  (testing "hearts are higher suit than diamonds"
+    (is (= [:heart :queen] (winner [:heart :queen] [:diamond :queen])))
+    (is (= [:heart :queen] (winner [:diamond :queen] [:heart :queen])))))
+
+; a game with only the lowest 6 cards in the deck
+(def game-steps '([([:club 4] [:spade 2] [:spade 3])
+                   ([:spade 4] [:club 2] [:club 3])]
+                  [([:spade 2] [:spade 3] [:club 4] [:spade 4])
+                   ([:club 2] [:club 3])]
+                  [([:spade 3] [:club 4] [:spade 4])
+                   ([:club 3] [:club 2] [:spade 2])]
+                  [([:club 4] [:spade 4])
+                   ([:club 2] [:spade 2] [:club 3] [:spade 3])]
+                  [([:spade 4] [:club 4] [:club 2])
+                   ([:spade 2] [:club 3] [:spade 3])]
+                  [([:club 4] [:club 2] [:spade 4] [:spade 2])
+                   ([:club 3] [:spade 3])]
+                  [([:club 2] [:spade 4] [:spade 2] [:club 4] [:club 3])
+                   ([:spade 3])]
+                  [([:spade 4] [:spade 2] [:club 4] [:club 3])
+                   ([:spade 3] [:club 2])]
+                  [([:spade 2] [:club 4] [:club 3] [:spade 4] [:spade 3])
+                   ([:club 2])]
+                  [([:club 4] [:club 3] [:spade 4] [:spade 3])
+                   ([:club 2] [:spade 2])]
+                  [([:club 3] [:spade 4] [:spade 3] [:club 4] [:club 2])
+                   ([:spade 2])]
+                  [([:spade 4] [:spade 3] [:club 4] [:club 2] [:club 3] [:spade 2])
+                   ()]))
+
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higer rank than jacks")
-  (testing "kings are higer rank than queens")
-  (testing "aces are higer rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+  (testing "game state transitions"
+    (is (= (nth game-steps 1) (apply play-round (nth game-steps 0))))
+    (is (= (nth game-steps 2) (apply play-round (nth game-steps 1))))
+    (is (= (nth game-steps 3) (apply play-round (nth game-steps 2))))
+    (is (= (nth game-steps 4) (apply play-round (nth game-steps 3))))
+    (is (= (nth game-steps 5) (apply play-round (nth game-steps 4))))
+    (is (= (nth game-steps 6) (apply play-round (nth game-steps 5))))
+    (is (= (nth game-steps 7) (apply play-round (nth game-steps 6))))
+    (is (= (nth game-steps 8) (apply play-round (nth game-steps 7))))
+    (is (= (nth game-steps 9) (apply play-round (nth game-steps 8))))
+    (is (= (nth game-steps 10) (apply play-round (nth game-steps 9))))
+    (is (= (nth game-steps 11) (apply play-round (nth game-steps 10))))))
+
+(def full-game [[[:club :ace]
+                 [:club :jack]
+                 [:spade 8]
+                 [:diamond 3]
+                 [:diamond :queen]
+                 [:spade :ace]
+                 [:club 2]
+                 [:club 7]
+                 [:heart 7]
+                 [:spade 4]
+                 [:heart 3]
+                 [:heart 9]
+                 [:heart :queen]
+                 [:spade 7]
+                 [:diamond :jack]
+                 [:diamond :ace]
+                 [:diamond 8]
+                 [:club 4]
+                 [:club 9]
+                 [:diamond 9]
+                 [:club 3]
+                 [:heart 8]
+                 [:heart :king]
+                 [:spade 2]]
+                [[:club :king]
+                 [:spade :king]
+                 [:diamond 5]
+                 [:spade 9]
+                 [:spade 6]
+                 [:club 5]
+                 [:club :queen]
+                 [:heart 6]
+                 [:diamond 6]
+                 [:heart :jack]
+                 [:spade 3]
+                 [:diamond :king]
+                 [:heart 4]
+                 [:heart 5]
+                 [:diamond 2]
+                 [:spade :queen]
+                 [:spade :jack]
+                 [:club 6]
+                 [:diamond 4]
+                 [:diamond 7]
+                 [:heart 2]
+                 [:heart :ace]
+                 [:club 8]
+                 [:spade 5]]])
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards")
-  (testing "if both players run out of cards then it is a draw"))
-
+  (testing "a bunch of random games (lol)"
+    (is (seq (apply play-game (deal cards))))
+    (is (seq (apply play-game (deal cards))))
+    (is (seq (apply play-game (deal cards))))
+    (is (seq (apply play-game (deal cards)))))
+  (testing "the player loses when they run out of cards"
+    (is (= "Player 2 wins" (apply play-game full-game)))))
