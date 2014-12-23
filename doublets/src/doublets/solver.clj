b0196d82d47e73abc0bd72f0e2bc2da05e16b44a
Aaron Stacy <aaron.r.stacy@gmail.com>
2014-12-22 21:56:47 -0600

    add a doublets solution
    (that would fail an interview)

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,25 @@
                (slurp)
                (read-string)))
 
+(defn valid-next-word [word other]
+  (if (not= (count word) (count other))
+    false
+    (->> (map vector word other)
+         (filter #(apply not= %))
+         (count)
+         (= 1))))
+
+(defn search [word1 word2]
+  (let [branch? (fn [doublet]
+                  not= (last doublet) word2)
+        children (fn [doublet]
+                   (->> words
+                        (filter #(and (valid-next-word (last doublet) %)
+                                      (not-any? (partial = %) doublet)))
+                        (map #(conj doublet %))))]
+    (tree-seq branch? children [word1])))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (->> (search word1 word2)
+      (filter #(= (last %) word2))
+      (first)))
--- doublets/test/doublets/solver_test.clj
+++ doublets/test/doublets/solver_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [doublets.solver :refer :all]))
 
+(deftest valid-next-word-test
+  (testing "valid next word"
+    (is (valid-next-word "cheat" "wheat"))
+    (is (valid-next-word "wheat" "cheat"))
+    (is (valid-next-word "cheat" "cheap")))
+  (testing "invalid next word"
+    (is (not (valid-next-word "foo" "bar"))))
+  (testing "invalid word because they are the same"
+    (is (not (valid-next-word "jay-z" "jay-z"))))
+  (testing "invalid word because they are not the same length"
+    (is (not (valid-next-word "hoof" "hooves")))
+    (is (not (valid-next-word "hooves" "hoof")))
+    (is (not (valid-next-word "kanye" "kanye west")))
+    (is (not (valid-next-word "kanye west" "kanye")))))
+
 (deftest solver-test
   (testing "with word links found"
     (is (= ["head" "heal" "teal" "tell" "tall" "tail"]
@@ -17,5 +32,6 @@
            (doublets "wheat" "bread"))))
 
   (testing "with no word links found"
-    (is (= []
-           (doublets "ye" "freezer")))))
+    (is (= nil
+           (doublets "ye" "freezer"))))
+  )
