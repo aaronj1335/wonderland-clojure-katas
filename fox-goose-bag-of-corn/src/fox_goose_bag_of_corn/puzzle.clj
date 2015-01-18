b1a60c5d8d651d102a93b750ce5625ad0509ffcb
Aaron Stacy <aaron.r.stacy@gmail.com>
2015-01-17 19:03:15 -0600

    half-baked and not working

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,163 @@
-(ns fox-goose-bag-of-corn.puzzle)
+(ns fox-goose-bag-of-corn.puzzle
+  (:require [clojure.pprint :refer [pprint]]))
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[:fox :goose :corn :you] [:boat] []])
+
+(def invalid-groups #{#{:fox :goose} #{:goose :corn}})
+
+(defn its-cool-chill
+  "return true if given a position, nothing's gonna get eaten"
+  [pos]
+  (not-any? invalid-groups (map set pos)))
+
+; (defn one-way-boat
+;   "return true if given a plan, you didn't get off the boat on the same shore
+;   you left. note this only checks the last position."
+;   [plan]
+;   (let [last-3-poss (take 3 (reverse plan))
+;         [prev prev2 prev3] (map #(first (positions :you %)) last-3-poss)]
+;     (or (not= prev2 1) (not= prev3 prev))))
+
+(defn progress
+  "return true if you didn't just undo your last action
+
+  this prevents doing something like bringing the goose to the far shore and
+  then right back to the near shore."
+  [plan]
+  (let [[prev prev2 prev3] (take 3 (reverse plan))]
+    (not= prev prev3)))
+
+(defn positions
+  "s/o to @ponzao http://stackoverflow.com/a/4831131/5377"
+  [pred coll]
+  (keep-indexed (fn [i x]
+                  (when (pred x)
+                    i))
+                coll))
+
+; (defn transitions
+;   "given a position, return the possible positions to which we can transition"
+;   [plan]
+
+;   (let [pos (last plan)
+;         prev-pos (second (reverse plan))
+
+;         your-idx (first (positions :you pos))
+;         prev-idx (when prev-pos (first (positions :you prev-pos)))
+
+;         ; "bringable" is all the items in your group that you can take (i.e.
+;         ; everything except you or the boat)
+;         bringable (disj (disj (nth pos your-idx) :you) :boat)]
+
+;     ; (print "in transitions\n")
+;     ; (pprint {:pos (last plan)
+;     ;          :prev-pos (second (reverse plan))
+
+;     ;          :your-idx (first (positions :you pos))
+;     ;          :prev-idx (when prev-pos (first (positions :you prev-pos)))
+
+;     ;          :bringable (disj (disj (nth pos your-idx) :you) :boat)
+;     ;          })
+
+;     (if (= your-idx 1)
+
+;       (if (= prev-idx 0)
+;         [[(nth pos 0) #{:boat} (conj (nth pos 2) :you (first bringable))]]
+;         [[(conj (nth pos 0) :you (first bringable)) #{:boat} (nth pos 2)]])
+
+;       (let [new-set (if (= your-idx 0)
+;                       (fn [bring]
+;                         [(disj bringable bring)
+;                          (conj #{:boat :you} bring)
+;                          (nth pos 2)])
+;                       (fn [bring]
+;                         [(nth pos 0)
+;                          (conj #{:boat :you} bring)
+;                          (disj bringable bring)]))]
+
+;         (map new-set bringable)))))
+
+(defn transitions
+  "given a position, return the possible positions to which we can transition"
+  [pos]
+
+  (let [
+        ; pos (last plan)
+        ; prev-pos (second (reverse plan))
+
+        your-idx (first (positions :you pos))
+        ; prev-idx (when prev-pos (first (positions :you prev-pos)))
+
+        ; "bringable" is all the items in your location that you can take (i.e.
+        ; everything except you or the boat)
+        bringable (disj (disj (nth pos your-idx) :you) :boat)]
+
+    ; (print "in transitions\n")
+    ; (pprint {:pos (last plan)
+    ;          :prev-pos (second (reverse plan))
+
+    ;          :your-idx (first (positions :you pos))
+    ;          :prev-idx (when prev-pos (first (positions :you prev-pos)))
+
+    ;          :bringable (disj (disj (nth pos your-idx) :you) :boat)
+    ;          })
+
+    (if (= your-idx 1)
+
+      [[(conj (nth pos 0) :you (first bringable)) #{:boat} (nth pos 2)]
+       [(nth pos 0) #{:boat} (conj (nth pos 2) :you (first bringable))]]
+
+      (let [new-set (if (= your-idx 0)
+                      (fn [bring]
+                        [(disj bringable bring)
+                         (conj #{:boat :you} bring)
+                         (nth pos 2)])
+                      (fn [bring]
+                        [(nth pos 0)
+                         (conj #{:boat :you} bring)
+                         (disj bringable bring)]))]
+
+        (map new-set bringable)))))
+
+(defn success [plan]
+  (= [#{} #{:boat} #{:you :fox :goose :corn}] (last plan)))
+
+; (pprint (transitions (map set start-pos)))
+; (pprint (transitions (map set [[:fox :corn]              [:boat :you :goose] []])))
+
+(defn search [pos]
+  (let [
+        branch? (complement success)
+        ; branch? (fn [plan]
+        ;           ; (print "============================================================\n")
+        ;           ; (print "BRANCHING ON:\n")
+        ;           ; (pprint plan)
+        ;           ; (print "------------------------------------------------------------\n")
+        ;           (its-cool-chill (last plan)))
+
+        children (fn [plan]
+                   (when (not (success plan))
+                     (print "============================================================\n")
+                     (print "YIELDING CHILDREN FOR:\n")
+                     (pprint plan)
+                     (print "............................................................\n")
+                     (pprint (filter (every-pred #(its-cool-chill (last %))
+                                         progress)
+                             (map #(conj plan %) (transitions (last plan)))))
+                     (print "------------------------------------------------------------\n")
+                     (filter (every-pred #(its-cool-chill (last %))
+                                         progress)
+                             (map #(conj plan %) (transitions (last plan))))
+                     ))
+
+        ]
+    (tree-seq branch? children [pos])))
 
 (defn river-crossing-plan []
-  start-pos)
+  (first (filter (fn [plan]
+                   (print "============================================================\n")
+                   (print "GOT PLAN:\n")
+                   (pprint plan)
+                   (print "------------------------------------------------------------\n")
+                   (success plan)
+                   ) (search (map set start-pos)))))
--- fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
+++ fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
@@ -1,45 +1,68 @@
 (ns fox-goose-bag-of-corn.puzzle-test
   (:require [clojure.test :refer :all]
-            [fox-goose-bag-of-corn.puzzle :refer :all]
-            [clojure.set]))
+            [fox-goose-bag-of-corn.puzzle :refer :all]))
 
-(defn validate-move [step1 step2]
-  (testing "only you and another thing can move"
-    (let [diff1 (clojure.set/difference step1 step2)
-          diff2 (clojure.set/difference step2 step1)
-          diffs (concat diff1 diff2)
-          diff-num (count diffs)]
-      (is (> 3 diff-num))
-      (when (pos? diff-num)
-        (is (contains? (set diffs) :you)))
-      step2)))
+(def solution
+  [[[:fox :goose :corn :you]  [:boat]             []]
+   [[:fox :corn]              [:boat :you :goose] []]
+   [[:fox :corn]              [:boat]             [:goose :you]]
+   [[:fox :corn]              [:boat :you]        [:goose]]
+   [[:fox :corn :you]         [:boat]             [:goose]]
+   [[:fox]                    [:corn :boat :you]  [:goose]]
+   [[:fox]                    [:boat]             [:goose :you :corn]]
+   [[:fox]                    [:boat :you :goose] [:corn]]
+   [[:fox :goose :you]        [:boat]             [:corn]]
+   [[:goose]                  [:boat :you :fox]   [:corn]]
+   [[:goose]                  [:boat]             [:corn :you :fox]]
+   [[:goose]                  [:boat :you]        [:corn :fox]]
+   [[:goose :you]             [:boat]             [:corn :fox]]
+   [[]                        [:boat :you :goose] [:corn :fox]]
+   [[]                        [:boat]             [:corn :fox :goose :you]]])
 
-(deftest test-river-crossing-plan
-  (let [crossing-plan (map (partial map set) (river-crossing-plan))]
-    (testing "you begin with the fox, goose and corn on one side of the river"
-      (is (= [#{:you :fox :goose :corn} #{:boat} #{}]
-             (first crossing-plan))))
-    (testing "you end with the fox, goose and corn on one side of the river"
-      (is (= [#{} #{:boat} #{:you :fox :goose :corn}]
-             (last crossing-plan))))
-    (testing "things are safe"
-      (let [left-bank (map first crossing-plan)
-            right-bank (map last crossing-plan)]
-        (testing "the fox and the goose should never be left alone together"
-          (is (empty?
-               (filter #(= % #{:fox :goose}) (concat left-bank right-bank)))))
-        (testing "the goose and the corn should never be left alone together"
-          (is (empty?
-               (filter #(= % #{:goose :corn}) (concat left-bank right-bank)))))))
-    (testing "The boat can carry only you plus one other"
-      (let [boat-positions (map second crossing-plan)]
-        (is (empty?
-             (filter #(> (count %) 3) boat-positions)))))
-    (testing "moves are valid"
-      (let [left-moves (map first crossing-plan)
-            middle-moves (map second crossing-plan)
-            right-moves (map last crossing-plan)]
-        (reduce validate-move left-moves)
-        (reduce validate-move middle-moves)
-        (reduce validate-move right-moves )))))
+(deftest test-its-cool-chill
+  (testing "it's not cool when the fox is with that goose tho"
+    (is (not (its-cool-chill [[:fox :goose] [:boat :you :corn] []]))))
+  (testing "it's not cool when the goose is with the corn tho"
+    (is (not (its-cool-chill [[:fox] [:boat :you] [:corn :goose]]))))
+  (testing "be it chill tho? yes it do."
+    (is (every? its-cool-chill solution))))
 
+; (deftest test-one-way-boat
+;   (testing "you don't get off the boat the same shore than you got on"
+;     (is (not (one-way-boat [[#{:goose :corn :you :fox} #{:boat} #{}]
+;                             [#{:fox :corn} #{:you :boat :goose} #{}]
+;                             [#{:goose :corn :you :fox} #{:boat} #{}]])))
+
+;     (is (not (one-way-boat [[#{:corn} #{:boat} #{:you :fox :goose}]
+;                             [#{:corn} #{:you :boat :goose} #{:fox}]
+;                             [#{:corn} #{:boat} #{:you :fox :goose}]]))))
+;   (testing "you get off the boat a different shore you got on"
+;     (is (not (one-way-boat [[#{:goose :corn :you :fox} #{:boat} #{}]
+;                             [#{:fox :corn} #{:you :boat :goose} #{}]
+;                             [#{:corn :fox} #{:boat} #{:you :goose}]])))
+
+;     (is (not (one-way-boat [[#{:corn} #{:boat} #{:you :fox :goose}]
+;                             [#{:corn} #{:you :boat :goose} #{:fox}]
+;                             [#{:corn :you :goose} #{:boat} #{:fox}]])))))
+
+(deftest test-transitions
+  (testing "transitions from the left shore"
+    (is (= (transitions (map set (first solution)))
+           '([#{:goose :corn} #{:you :fox :boat} #{}]
+             [#{:fox :corn} #{:you :boat :goose} #{}]
+             [#{:fox :goose} #{:you :boat :corn} #{}]))))
+  (testing "transitions from the boat"
+    (is (= (transitions (map set (second solution)))
+           '([#{:goose :corn :fox :you} #{:boat} #{}]
+             [#{:fox :corn} #{:boat} #{:you :goose}]))))
+  (testing "transitions from the right shore"
+    (is (= (transitions (map set (nth solution 2)))
+           '([#{:fox :corn} #{:you :boat :goose} #{}])))))
+
+
+(run-tests)
+
+; (deftest test-river-crossing-plan
+;   (testing "the fox, goose, corn, and you all made it to the other side of the river"
+;     (is (= (map set solution)
+;            (map set (river-crossing-plan))))))
